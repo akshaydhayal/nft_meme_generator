9b1fd2a0a5158360021564780fe4dabc6526d871
akshaydhayal <akshaydhayalkd99@gmail.com>
2023-01-23 15:14:54 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+const url = "https://api.helius.xyz/v0/addresses/CtoUM5HKxAmLUTCCuYyHftrq7jT6oHpTN9yaDGUuhp3d/nfts?api-key=f2d4e996-c072-4fd8-9e62-a4f9a7540d58"
+const getNftDetails= async () => {
+  const { data } = await axios.get(url)
+  console.log("parsed transactions: ", data)
+}
+getNftDetails()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Hello</h1>
+          </div>
   );
 }
 
